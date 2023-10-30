0d8f60f37799f332d978b476efa4e62d8588e35b
saipranay47 <saipranayadepu07@gmail.com>
2023-10-30 18:05:42 +0530

    landing page base

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1 className="underline">hello world</h1>
+    <div className="bg-black min-h-screen flex w-full justify-center items-center text-white flex-col">
+      <div className="flex gap-4 flex-col items-center">
+        <a
+          href="https://saipranay.vercel.app/"
+          target="_blank"
+          className=" px-6 py-3 bg-transparent text-white border border-white rounded-full w-max"
+        >
+          Created by Sai Pranay.
+        </a>
+        <h1 className="text-center text-6xl tracking-tighter font-bold">
+          Awwwards <br /> Sticky Bottom Navigation
+        </h1>
+        <p className="text-[#666] ">
+          Explore your website with this beautiful sticky bottom navigation
+          inspired by Awwwards.com.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/StickyBottomNavigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function StickyBottomNavigation() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default StickyBottomNavigation
